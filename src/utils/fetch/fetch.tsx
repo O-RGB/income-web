44dd508bf2d27afbf7712bd4f0f80198ec60eeb6
O-RGB <ok.oza5555@gmail.com>
2024-05-30 22:18:57 +0700

    NEXT_PUBLIC_VERCEL_URL

--- src/utils/fetch/fetch.tsx
+++ src/utils/fetch/fetch.tsx
@@ -1,5 +1,29 @@
 const apiUrl = process.env.API_URL;
 
+function getDomain() {
+  const environment = process.env.NEXT_PUBLIC_VERCEL_ENV;
+  const isProduction = environment === "production";
+  const isStaging = environment === "staging";
+  const protocol = isProduction || isStaging ? "https" : "http";
+
+  const domain = process.env.NEXT_PUBLIC_VERCEL_URL
+    ? process.env.NEXT_PUBLIC_VERCEL_URL
+    : "localhost:3000";
+
+  if (!isProduction && domain === "localhost:3000") {
+    return `${protocol}://${domain}`;
+  }
+
+  const urlObject = new URL(`${protocol}://${domain}`);
+  const hostnameSplit = urlObject.hostname.split(".");
+  const subdomain = hostnameSplit[0].split("-")[0];
+  const secondLevelDomain = hostnameSplit[1];
+  const topLevelDomain = hostnameSplit[2];
+
+  const finalDomain = `${subdomain}.${secondLevelDomain}.${topLevelDomain}`;
+  return `${protocol}://${finalDomain}`;
+}
+
 interface IFetcher<Input = any, query = any> {
   data?: Input | query;
   method?: "GET" | "POST";
@@ -31,6 +55,9 @@ export async function Fetcher<Input = any, Result = any>(
   loading?: (load: boolean) => void,
   headers: any = undefined
 ) {
+
+  var realUrl = getDomain();
+  console.log(realUrl)
   //LOAD
   loading?.(true);
   //GET
@@ -48,12 +75,6 @@ export async function Fetcher<Input = any, Result = any>(
   return await fetch(url, {
     method: param?.method ? param.method : "GET",
     body: formParam,
-    // headers: headers,
-    // mode: "no-cors",
-    cache: "no-store",
-    headers: new Headers({
-      "Content-Type": "text/plain",
-    }),
   })
     .then((response) => {
       return response.json() as IGeneralReturnFetch<Result>;
