2040391c534b758c8594a02311695c9a2e75af90
O-RGB <ok.oza5555@gmail.com>
2024-05-30 22:28:45 +0700

    vercel

--- src/utils/fetch/fetch.tsx
+++ src/utils/fetch/fetch.tsx
@@ -1,29 +1,5 @@
 const apiUrl = process.env.API_URL;
 
-function getDomain() {
-  const environment = process.env.NEXT_PUBLIC_VERCEL_ENV;
-  const isProduction = environment === "production";
-  const isStaging = environment === "staging";
-  const protocol = isProduction || isStaging ? "https" : "http";
-
-  const domain = process.env.NEXT_PUBLIC_VERCEL_URL
-    ? process.env.NEXT_PUBLIC_VERCEL_URL
-    : "localhost:3000";
-
-  if (!isProduction && domain === "localhost:3000") {
-    return `${protocol}://${domain}`;
-  }
-
-  const urlObject = new URL(`${protocol}://${domain}`);
-  const hostnameSplit = urlObject.hostname.split(".");
-  const subdomain = hostnameSplit[0].split("-")[0];
-  const secondLevelDomain = hostnameSplit[1];
-  const topLevelDomain = hostnameSplit[2];
-
-  const finalDomain = `${subdomain}.${secondLevelDomain}.${topLevelDomain}`;
-  return `${protocol}://${finalDomain}`;
-}
-
 interface IFetcher<Input = any, query = any> {
   data?: Input | query;
   method?: "GET" | "POST";
@@ -55,9 +31,6 @@ export async function Fetcher<Input = any, Result = any>(
   loading?: (load: boolean) => void,
   headers: any = undefined
 ) {
-
-  var realUrl = getDomain();
-  console.log(realUrl)
   //LOAD
   loading?.(true);
   //GET
@@ -65,11 +38,14 @@ export async function Fetcher<Input = any, Result = any>(
     param?.method === "GET" || param?.method === undefined
       ? url + QueryGet(param?.data)
       : url;
+
+  const vercel = process.env.VERCEL_URL;
+  url = url + `${vercel !== undefined ? vercel : ""}`;
   //POST
   var formParam: FormData | undefined =
     param?.method === "POST" ? QueryPost(param.data) : undefined;
 
-  console.log(apiUrl);
+  console.log(url);
 
   //FETCH
   return await fetch(url, {
