141246e4349007ba5b249e2ae6d90e39f3006d06
O-RGB <ok.oza5555@gmail.com>
2024-06-01 10:31:13 +0700

    remove cors

--- src/utils/fetch/fetch.tsx
+++ src/utils/fetch/fetch.tsx
@@ -52,11 +52,11 @@ export async function Fetcher<Input = any, Result = any>(
   return await fetch(url, {
     method: param?.method ? param.method : "GET",
     body: formParam,
-    cache: "no-store",
-    mode: "cors",
-    headers: {
-      "Access-Control-Allow-Origin": "*",
-    },
+    // cache: "no-store",
+    // mode: "cors",
+    // headers: {
+    //   "Access-Control-Allow-Origin": "*",
+    // },
   })
     .then((response) => {
       return response.json() as IGeneralReturnFetch<Result>;
