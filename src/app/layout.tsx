7d663202d322a918a587bb57219cce3c2c328156
O-RGB <ok.oza5555@gmail.com>
2024-07-10 14:56:34 +0700

    gew

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -61,7 +61,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" href="/favicon.ico" sizes="32x32" />
         <link
           rel="icon"
           href="/icon?<generated>"
